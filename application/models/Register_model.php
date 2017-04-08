91c7e8e7ad4e26dc1774fd2ecc82836b57847d7a
christian7660 <christian_7660@hotmail.com>
2017-04-07 19:40:58 -0500

    notas

---
+++ application/models/Register_model.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Register_model extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+	public function register_user($data) {
+		if ($this->db->insert('register', $data)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public function update_user($data, $email) {
+		$this->db->where('user_email', $email);
+		$this->db->update('register', $data);
+	}
+}
